60f7b48e2fc45eaa96e115ba29dee356f9012aa4
evangelina <evangelina.gomes@ellecktra.com>
2020-11-20 21:18:36 -0300

    test get users json

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const getUsuarios = require('./api/usuarios.js');
+//const getUsuarios = require('./api/usuarios.js');
+const usuarios = require('./data/person-data');
 //app.use(express.static('data'));
 
 //REST API
-app.get('/usuarios', function (req, res){
+/*app.get('/usuarios', function (req, res){
     res.send(getUsuarios());
-})
+})*/
+
+app.get('/usuarios', (req, res) =>  res.json(usuarios));
+
+
 app.listen(port, () => {
     console.log('Radium app listening at http://localhost:${port}');
 })
