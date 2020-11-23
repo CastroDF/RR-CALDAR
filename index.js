6547b720f2435baddbcedc28de9adad6e824b1e6
Diego Fernando Castro <CastroDiegoFernando00@gmail.com>
2020-11-23 17:57:26 -0300

    RR-CALDAR: Reverting wrong commits

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-const getUsuarios = require('./api/usuarios.js');
-//app.use(express.static('data'));
-
-//REST API
-app.get('/usuarios', function (req, res){
-    res.send(getUsuarios());
-})
-app.listen(port, () => {
-    console.log('Radium app listening at http://localhost:${port}');
-})
+console.log("Hi");
