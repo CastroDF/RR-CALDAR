e655ba875037c87dc1875081f6c01f2a1e514cfa
Nicolas Mandrile <mandrilenico@gmail.com>
2020-11-24 11:09:35 -0300

    Fix some problems

--- routes/api/technicians.js
+++ routes/api/technicians.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const technicians = require('../../data/technicians-data');
+const url = require('url')
+
 //this are only methods for get an answer
 //get all technicians
 router.get('/', (req, res) => {
@@ -17,6 +19,29 @@ router.get('/:id', (req, res, next) =>{
     } else {
         //next() 
         res.status(400).json({msg: `Member not found ${req.params.id} responde por id`});//an answerwhen the response fail
+    }
+});
+
+//Get a technician by attribute
+// get by email
+router.get('/getby/:email', (req, res) =>{//?Score=${score} /getby/:email' 
+    //queryObject = url.parse(req.url,true).pathname;
+    console.log('el objeto', url.parse(req.url,true).query);  
+    const found = technicians.some(technical => technical.email === req.params.email);
+    if(found){
+        res.json(technicians.filter(technical => technical.email === req.params.email)); 
+    } else {
+        res.status(400).json({msg: `Member not found ${req.params.email}`});//an answerwhen the response fail
+    }
+});
+//get by Name
+router.get('/getby/:firstname', (req, res) =>{
+    // res.send(req.params.email);
+    const found = technicians.some(technical => technical.firstname === req.params.firstname);
+    if(found){
+        res.json(technicians.filter(technical => technical.firstname === req.params.firstname)); 
+    } else {
+        res.status(400).json({msg: `Member not found ${req.params.firstname}`});//an answerwhen the response fail
     }
 });
 
