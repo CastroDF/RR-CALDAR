e2ac4b70d36dedfcb057dbd92f9adebb80dce019
Facu de Lafuente <62487681+FacundoDLF@users.noreply.github.com>
2020-11-24 19:34:50 -0300

    Fixe

--- Customers/index_customers.js
+++ Customers/index_customers.js
@@ -1,70 +1,60 @@
-const { request, response } = require('express');
 const express = require('express');
-const { dirname } = require('path');
-const customers = require('./data_customers.json');
-const path = require('path');
-
 const app = express();
+const path = require('path');
+const customers = require('./data_customers/data_customers.json');
+const PORT = process.env.PORT || 5000;
+
 
 app.listen(8080, () => {
     console.log('Server:8080 OK')
 });
 
-
 app.get('/customers', (request, response) => {
-    console.log(__dirname)
-    response.sendFile(path.resolve(__dirname, 'customers.html'))
+    console.log('Welcome to Customers page')
+    response.sendFile(path.resolve('../Customers/customers.html'))
 });
 
 
+app.get('/customers/all', (request, response) => {
+    console.log('All customers here')
+    response.json(customers),
+    response.sendFile('../Customers/customers.html')
+});
 
 
+app.get('/customers/:id_customer', (request, response) => {
+    console.log(customers)
+    const found = customers.some(customers => customers.id_customer === parseInt(request.params.id_customer));
+    if(found) {
+        response.json(customers.filter(customers => customers.id_customer === parseInt(request.params.id_customer))
+        );
+    } else {
+        response.status(400).json({ msg:'Ups! Customer not found with that ID' });
+    }
+});
+
+app.get('/customers/type/:type', (request, response) => {
+    console.log(customers)
+    const found = customers.some(customers => customers.type === (request.params.type));
+    if(found) {
+        response.json(customers.filter(customers => customers.type === (request.params.type))
+        );
+    } else {
+        response.status(400).json({ msg:'Ups! Customer not found with that Attribute' });
+    }
+});
 
 
-    app.get('/customers/all', (request, response) => {
-        console.log(__dirname)
-        response.sendFile(path.resolve(__dirname, 'data_customers.json'))
-    });
-
-
-    app.get('/customers/:id_customer', (request, response) => {
-        console.log(__dirname)
-        const found = customers.some(customers => customers.id_customer === parseInt(request.params.id_customer));
-
-        if(found) {
-            response.json(customers.filter(customers => customers.id_customer === parseInt(request.params.id_customer))
-            );
-        } else {
-            response.status(400).json({ msg:'Ups! Customer not found with that ID' });
-        }
-    });
-
-
-    app.get('/customers/type/:type', (request, response) => {
-        console.log(__dirname)
-        const found = customers.some(customers => customers.type === (request.params.type));
-
-        if(found) {
-            response.json(customers.filter(customers => customers.type === (request.params.type))
-            );
-        } else {
-            response.status(400).json({ msg:'Ups! Customer not found with that Attribute' });
-        }
-    });
-
-
-
-
-    app.delete('/customers/delete/:id_customer', (request, response) => {
-        console.log(__dirname)
-        const found = customers.some(customers => customers.id_customer === parseInt(request.params.id_customer));
-
-        if(found) {
-            response.json({
-                msg: 'Customer Deleted',
-                customers: customers.filter(customers => customers.id_customer !== parseInt(request.params.id_customer))
-            });
-        } else {
-            response.status(400).json({ msg:'Ups! Customer not found with that ID' });
-        }
-    });
+app.delete('/customers/delete/:id_customer', (request, response) => {
+    console.log('Deleting Customer..')
+    const found = customers.some(customers => {
+    customers.id_customer === parseInt(request.params.id_customer)
+    })
+    if(found) {
+        response.json({
+            msg: 'Customer ${request.params.id_customer} Deleted',
+        });
+    } else {
+        response.status(400).json({ msg:'Ups! Customer not found with ID ${req.params.id}' });
+    }
+});
