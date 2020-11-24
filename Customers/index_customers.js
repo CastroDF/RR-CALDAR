f702eb25a70c6bd23fea157ddfa312f03b822680
Facu de Lafuente <62487681+FacundoDLF@users.noreply.github.com>
2020-11-24 19:59:02 -0300

    Delete Logs & Fix Customers All

--- Customers/index_customers.js
+++ Customers/index_customers.js
@@ -8,21 +8,14 @@ app.listen(8080, () => {
     console.log('Server:8080 OK')
 });
 
+
 app.get('/customers', (request, response) => {
-    console.log('Welcome to Customers page')
-    response.sendFile(path.resolve('../Customers/customers.html'))
-});
-
-
-app.get('/customers/all', (request, response) => {
-    console.log('All customers here')
     response.json(customers),
     response.sendFile('../Customers/customers.html')
 });
 
 
 app.get('/customers/:id_customer', (request, response) => {
-    console.log(customers)
     const found = customers.some(customers => customers.id_customer === parseInt(request.params.id_customer));
     if(found) {
         response.json(customers.filter(customers => customers.id_customer === parseInt(request.params.id_customer))
@@ -33,7 +26,6 @@ app.get('/customers/:id_customer', (request, response) => {
 });
 
 app.get('/customers/type/:type', (request, response) => {
-    console.log(customers)
     const found = customers.some(customers => customers.type === (request.params.type));
     if(found) {
         response.json(customers.filter(customers => customers.type === (request.params.type))
@@ -45,7 +37,6 @@ app.get('/customers/type/:type', (request, response) => {
 
 
 app.delete('/customers/delete/:id_customer', (request, response) => {
-    console.log('Deleting Customer..')
     const found = customers.some(customers => {
     customers.id_customer === parseInt(request.params.id_customer)
     })
