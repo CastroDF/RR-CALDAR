54d0de79e369ef0206c24e0c76218d7fecede864
Nicolas Mandrile <mandrilenico@gmail.com>
2020-11-24 18:35:06 -0300

    getTechniciansByAttribute was added and delete some comments in th code

--- controllers/technicians.js
+++ controllers/technicians.js
@@ -26,29 +26,70 @@ app.get('/technicians/getById/:id', (req, res, next) =>{
 });
 
 //Get a technician by attribute
-// get by email
-app.get('/technicians/getTechniciansByAttribute/email:email', (req, res) =>{//?Score=${score} /getby/:email' 
-    queryObject = url.parse(req.url,true).query;
-    console.log('el objeto', queryObject);  
-    const found = technicians.some(technical => technical.email === req.params.email);
-    if(found){
-        res.json(technicians.filter(technical => technical.email === req.params.email)); 
-    } else {
-        res.status(400).json({msg: `Member not found ${req.params.email} sale por aca`});//an answerwhen the response fail
+app.get('/technicians/getByAttribute/:type/:data', (req, res) =>{
+    //queryObject = url.parse(req.url).search;
+    console.log('el objeto', req.params.type);
+    switch(req.params.type){
+        case 'email':
+            var found = technicians.some(technical => technical.email === req.params.data);
+            if(found){
+                res.json(technicians.filter(technical => technical.email === req.params.data));
+            } else {
+                res.status(400).json({msg: `Member not found ${req.params.email} sale por aca`});//an answerwhen the response fail
+            }
+            break;
+        case 'first_name':
+            found = technicians.some(technical => technical.first_name === req.params.data);
+            if(found){
+                res.json(technicians.filter(technical => technical.first_name === req.params.data));
+            } else {
+                res.status(400).json({msg: `Member not found ${req.params.data}`});
+            }
+            break;
+        case 'last_name':
+            found = technicians.some(technical => technical.last_name === req.params.data);
+            if(found){
+                res.json(technicians.filter(technical => technical.last_name === req.params.data));
+            } else {
+                res.status(400).json({msg: `Member not found ${req.params.data}`});
+            }
+            break;
+        case 'typeIds':
+            found = technicians.some(technical => technical.typeIds === parseInt(req.params.data));
+            if(found){
+                res.json(technicians.filter(technical => technical.typeIds === parseInt(req.params.data)));
+            } else {
+                res.status(400).json({msg: `Member not found ${req.params.data}`});
+            }
+            break;
+        case 'skillsId':
+            found = technicians.some(technical => technical.skillsId === req.params.data);
+            if(found){
+                res.json(technicians.filter(technical => technical.skillsId === req.params.data));
+            } else {
+                res.status(400).json({msg: `Member not found ${req.params.data}`});
+            }
+            break;
+        case 'daily_capacity':
+            found = technicians.some(technical => technical.daily_capacity === req.params.data);
+            if(found){
+                res.json(technicians.filter(technical => technical.daily_capacity === req.params.data));
+            } else {
+                res.status(400).json({msg: `Member not found ${req.params.data}`});
+            }
+            break;
+        case 'hour_rate':
+            found = technicians.some(technical => technical.hour_rate === req.params.data);
+            if(found){
+                res.json(technicians.filter(technical => technical.hour_rate === req.params.data));
+            } else {
+                res.status(400).json({msg: `Member not found ${req.params.data}`});
+            }
+            break;
     }
 });
-// //get by Name
-// router.get('/getby/:firstname', (req, res) =>{
-//     // res.send(req.params.email);
-//     const found = technicians.some(technical => technical.firstname === req.params.firstname);
-//     if(found){
-//         res.json(technicians.filter(technical => technical.firstname === req.params.firstname)); 
-//     } else {
-//         res.status(400).json({msg: `Member not found ${req.params.firstname}`});//an answerwhen the response fail
-//     }
-// });
 
-//Delete a technician by Id
+//Delete a technician only by Id
 app.delete('/technicians/delete/:id', (req, res) =>{
     const found = technicians.some(member => member.id === parseInt(req.params.id));
     if(found){
