befb2bd50866485e9b37ed16be561228fc028d94
Diego Fernando Castro <CastroDiegoFernando00@gmail.com>
2020-11-30 12:20:22 -0300

    RR-CALDAR: Building controller refactor

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 
 // Controllers
-const boilerTypesController = require("../controllers/boiler-types-controllers.js");
+const boilerTypesController = require("../controllers/boiler-types-controller");
+const boilerTypesController = require("../controllers/building-controller");
 
 // Constants
 const PORT = 4000;
@@ -20,7 +21,9 @@ app.use(bodyParser.json());
 // Support parsing of application/x-www-form-urlencoded post data
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Controllers
 app.use("/boiler-types", boilerTypesController);
+app.use("/building", boilerTypesController);
 
 app.listen({ port: PORT }, () => {
   console.log(`Server running on http://localhost:${PORT}`);
