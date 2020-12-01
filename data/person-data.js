53dfa29dd5e788b70212324a8299970e35aa7097
evangelina <evangelina.gomes@ellecktra.com>
2020-12-01 16:57:49 -0300

    corrections errors merge

---
+++ data/person-data.js
@@ -0,0 +1,3 @@
+[{"id":1,"first_name":"Niven","last_name":"Szepe","email":"nszepe0@tumblr.com","gender":"Male","ip_address":"88.8.222.246"},
+{"id":2,"first_name":"Darcy","last_name":"Melloi","email":"dmelloi1@tumblr.com","gender":"Female","ip_address":"95.0.209.12"},
+{"id":3,"first_name":"Taffy","last_name":"Camelia","email":"tcamelia2@gnu.org","gender":"Female","ip_address":"158.238.219.178"}]
