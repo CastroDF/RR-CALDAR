85f777e09b073626a48ff2302d329a31e79e657d
evangelina <evangelina.gomes@ellecktra.com>
2020-12-01 17:11:55 -0300

    corrections errors

--- data/person-data.js
+++ data/person-data.js
@@ -1,3 +1,5 @@
-[{"id":1,"first_name":"Niven","last_name":"Szepe","email":"nszepe0@tumblr.com","gender":"Male","ip_address":"88.8.222.246"},
+const users = [{"id":1,"first_name":"Niven","last_name":"Szepe","email":"nszepe0@tumblr.com","gender":"Male","ip_address":"88.8.222.246"},
 {"id":2,"first_name":"Darcy","last_name":"Melloi","email":"dmelloi1@tumblr.com","gender":"Female","ip_address":"95.0.209.12"},
-{"id":3,"first_name":"Taffy","last_name":"Camelia","email":"tcamelia2@gnu.org","gender":"Female","ip_address":"158.238.219.178"}]
+{"id":3,"first_name":"Taffy","last_name":"Camelia","email":"tcamelia2@gnu.org","gender":"Female","ip_address":"158.238.219.178"}];
+
+module.exports = users;
