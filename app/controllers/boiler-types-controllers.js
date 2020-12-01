a5b3f488b9efd9bffbecdef51ead9d6b52289069
evangelina <evangelina.gomes@ellecktra.com>
2020-12-01 17:26:56 -0300

    corrections errors

---
+++ app/controllers/boiler-types-controllers.js
@@ -0,0 +1,163 @@
+const db = require("../models");
+const BoilerTypes = db.boilerTypes;
+
+exports.create = (req, res)  => {
+    if(!req.body.skillsId || !req.body.description || !req.body.stock){
+        return res.status(400).send({
+            message: "Data to create can not be empty"
+        });
+    }
+    const newBoilerTypes = new Boilertypes({
+        id: req.body.id,
+        skillsId: req.body.skillsId,
+        description: req.body.description,
+        stock: req.body.stock
+    });
+    newBoilerTypes
+        .save(newBoilerTypes)
+        .then(data =>{
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error ocurred while creating de Boiler Types"
+            });
+        });
+};
+
+exports.findAll = (req, res) => {
+    BoilerTypes.find({})
+        .then(data =>{
+            res.send(data);
+        })
+        .catch(err =>{
+            res.status(500).send({
+                message:
+                    err.message || "Some error ocurred while retrieving boiler types"
+            });
+        });
+};
+
+exports.findOne = (req, res) =>{
+    BoilerTypes.findOne({id: req.params.id})
+        .then(data =>{
+            if(!data){
+                return res.status(404).send({
+                    message: `Boiler types with id ${req.params.id} was not found`
+                })
+            }
+            res.send(data)
+        })
+        .catch(err =>{
+            res.status(500).send({
+                message:
+                    err.message || "Some error ocurred while retrieving boiler types"
+            });
+        });
+};
+
+exports.update = (req, res) => {
+    if(!req.body){
+        return res.status(400).send({
+            message: "Data to update can not be empty"
+        });
+    }
+
+    const id = req.params.id;
+
+    BoilerTypes.findOneAndUpdate({id}, req.body, {useFindAndModify: false})
+        .then(data =>{
+            if(!data){
+                res.status(404).send({
+                    message: "Cannot update boiler types with id = ${id}. Maybe boiler types was not found!"
+                });
+            }else{
+                res.send({
+                    message: `boiler types was updated succesfully`
+                });
+            }
+        })
+}
+
+exports.delete = (req, res) =>{
+    const id = req.params.id;
+    BoilerTypes.findOneAndRemove({id}, { useFindAndModify: false })
+        .then(data =>
+            res.send({ message: "Boiler Types was removed successifully"})
+        )
+        .catch(err =>{
+            res.status(500).send({
+                message: "Error removing boiler types with id = "+  id
+            });
+        });
+};
+
+
+/* router.get('/', (req, res) =>  res.json(boilerTypes));
+
+router.get('/:id', (req, res) => {
+    const found = boilerTypes.some(boilerType => boilerType.id === parseInt(req.params.id));
+    if(found){
+        res.json(boilerTypes.filter(boilerType => boilerType.id === parseInt(req.params.id)));
+        res.json(req.params.id);
+    }else{
+        res.status(400).json({msg: `No member with the id of ${req.params.id}`});
+    }
+});
+
+router.get('/delete/:id', (req, res) => {
+    const found = boilerTypes.some(boilerType => boilerType.id === parseInt(req.params.id));
+    if(found){
+        var index = boilerTypes.map((boilerType) => boilerType.id ).indexOf(parseInt(req.params.id));
+        if (index !== -1) {
+            boilerTypes.splice(index, 1);
+            res
+                .status(200)
+                .json({msj: `Member with id: ${req.params.id} was deleted`});
+        }
+    }else{
+        res.status(400).json({msg: `No member with the id of ${req.params.id}`});
+    }
+});
+
+
+router.get("/getBoilerTypesSkill/:skillsId", (req, res) => {
+    const skillsIdNumber = parseInt(req.params.skillsId);
+    const found = boilerTypes.some(boilerTypes => boilerTypes.skillsId.includes(skillsIdNumber));
+    console.log(found);
+    if (found) {
+        res.json(boilerTypes.filter(boilerTypes => {
+            boilerTypes.skillsId.includes(skillsIdNumber)
+        }))
+    } else {
+        res.status(400).json({ msg: `No boiler type with the skill id of ${req.params.skillsId}` })
+    }
+});
+
+
+router.get("/getBoilerTypesDescription/:description", (req, res) => {
+    const found = boilerTypes.some(boilerTypes => boilerTypes.description.includes(req.params.description));
+    if (found) {
+        res.json(boilerTypes.some(boilerTypes => {
+            boilerTypes.description.includes(req.params.description)
+                .status(200)
+                .json({msj: `Member with id: ${req.params.id} was deleted`});
+        }));
+    } else {
+        res.status(400).json({ msg: `No boiler type with the description of ${req.params.description}` })
+    }
+});
+
+
+router.get("/getBoilerTypesStock/:stock", (req, res) => {
+    const found = boilerTypes.some(boilerType => boilerType.stock === parseInt(req.params.stock));
+    if(found){
+        res.json(boilerTypes.filter(boilerType => boilerType.stock === parseInt(req.params.stock)));
+        res.json(req.params.stock);
+    }else{
+        res.status(400).json({msg: `No member with the id of ${req.params.id}`});
+    }
+});
+
+module.exports = router; */
--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const express = require("express");
+// App
+const app = express();
+// Constants
+const PORT = 4000;
+//db
+const db = require("./app/models");
+const router = require("./app/routes");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 // Controllers
 const boilerTypesController = require('./controllers/boiler-types-controller');
@@ -53,11 +60,30 @@ app.use(cors());
 // Support parsing of application/json type post data
 app.use(bodyParser.json());
 
+
 // Support parsing of application/x-www-form-urlencoded post data
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/boiler-types", boilerTypesController);
 
+
+//mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("conectedto the database");
+  })
+  .catch(err =>{
+    console.log("cannot connect to the database!", err);
+    process.exit;
+  })
+
+
+
+app.use(router);
+//app.use('/boiler-types', boilerTypesController);
+
 app.listen({ port: PORT }, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
 });
