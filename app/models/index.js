c7ae14467e96f60e44040c8f6bb98b616baf3f21
evangelina <evangelina.gomes@ellecktra.com>
2020-12-02 22:46:57 -0300

    last changes

--- app/models/index.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const db = {}
-db.mongoose = mongoose;
-db.url = "mongodb+srv://caldar:caldar1234@cluster0.ic0eo.mongodb.net/caldar?retryWrites=true&w=majority";
-db.boilerTypes = require("./boiler-types.js")(mongoose);
-db.appointments = require("./appointments.js")(mongoose);
-
-module.exports = db;
