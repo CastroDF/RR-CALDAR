b44cbf0f8ab0c489f2fcb49d70204b3573485b9f
evangelina <evangelina.gomes@ellecktra.com>
2020-12-07 15:22:44 -0300

    add appointments and boiler-types

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,14 @@ const router = require('express').Router();
 
 router.use('/technicians', techniciansRouter);
 
+const { routeBoilerTypes } =  require('./boiler-types');
+const boilerTypesRouter = require('./boiler-types');
+
+const { routeAppointments } =  require('./appointments');
+const appointmentsRouter = require('./appointments');
+
+var router = require("express").Router();
+router.use('/boiler-types', boilerTypesRouter);
+router.use('/appointments', appointmentsRouter);
+
 module.exports = router;
